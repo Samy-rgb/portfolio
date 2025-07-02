742d1948a2f198805bcf9d5af3626e983b96d090
Samy-rgb <workitsam01@gmail.com>
2025-07-02 15:33:17 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,137 @@
+export const servicesData = [
+    {
+        title: "FullStack Development",
+        description:
+            "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture and seamless integrations—ensuring reliability at every layer.",
+        items: [
+            {
+                title: "Backend",
+                description: "(MongoDB, Authentication)",
+            },
+            {
+                title: "Frontend",
+                description: "(React, Vite, TypeScript, Interactive UI/UX)",
+            },
+            {
+                title: "Database",
+                description: "(SQL/NoSQL Optimization, Scalable Structures)",
+            },
+        ],
+    },
+    {
+        title: "DevOps & Cloud Solutions",
+        description:
+            "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
+        items: [
+            {
+                title: "CI/CD Pipelines",
+                description: "(GitHub Actions, Docker, Kubernetes)",
+            },
+            {
+                title: "Server Management ",
+                description: "(Linux, Nginx, Load Balancing)",
+            },
+            {
+                title: "Performance Tuning",
+                description: "(Caching, Compression, Lighthouse 90+ Scores)",
+            },
+        ],
+    },
+
+    {
+        title: "WebDev",
+        description:
+            "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web and mobile apps (React Native/Flutter) that users love—bridging design and functionality seamlessly.",
+        items: [
+            {
+                title: "E-Commerce",
+                description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+            },
+        ],
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        name: "Apple landing page",
+        description:
+            "An online store specializing in phone accessories including cases, chargers, cables, and power banks with MagSafe compatibility.",
+        href: "https://applebysamyak.vercel.app/",
+        image: "/assets/projects/apple.jpg",
+        bgImage: "/assets/backgrounds/blanket.jpg",
+        frameworks: [
+            { id: 1, name: "React" },
+            { id: 2, name: "Vite" },
+            { id: 3, name: "GSAP" },
+            { id: 4, name: "Three.js" },
+            { id: 5, name: "Tailwind CSS" },
+        ],
+    },
+    {
+        id: 2,
+        name: "Zen",
+        description:
+            "A website where gaming meets advance technology",
+        href: "https://zenbysamyak.vercel.app/",
+        image: "/assets/projects/zen.jpg",
+        bgImage: "/assets/backgrounds/curtains.jpg",
+        frameworks: [
+            { id: 1, name: "React" },
+            { id: 2, name: "Vite" },
+            { id: 3, name: "GSAP" },
+            { id: 4, name: "Tailwind CSS" },
+        ],
+    },
+    {
+        id: 3,
+        name: "Mojito",
+        description:
+            "Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes designed to delight your senses.",
+        href: "https://mojitobysamyak.vercel.app/",
+        image: "/assets/projects/mojito.jpg",
+        bgImage: "/assets/backgrounds/map.jpg",
+        frameworks: [
+            { id: 1, name: "React" },
+            { id: 2, name: "Three.js" },
+            { id: 3, name: "Vite" },
+            { id: 4, name: "Tailwindcss" },
+            { id: 5, name: "GSAP" },
+        ],
+    },
+    {
+        id: 4,
+        name: "Cote Royale",
+        description:
+            "An expression of quiet luxury, Côte Royale is designed for the man who commands attention without seeking it. A reflection of nature’s raw beauty.",
+        href: "https://fragrancebysamyak.vercel.app/",
+        image: "/assets/projects/cote royale.jpg",
+        bgImage: "/assets/backgrounds/poster.jpg",
+        frameworks: [
+            { id: 1, name: "GSAP" },
+            { id: 2, name: "Next.js" },
+            { id: 3, name: "React" },
+            { id: 4, name: "Tailwindcss" },
+        ],
+    },
+    {
+        id: 5,
+        name: "Fizzi",
+        description:
+            "Made with only the best natural ingredients, our soda is free from artificial sweeteners and flavors. It’s a crisp, clean taste that feels as good as it tastes, giving you a boost of real, natural refreshment.",
+        href: "",
+        image: "/assets/projects/fizzi.jpg",
+        bgImage: "/assets/backgrounds/table.jpg",
+        frameworks: [
+            { id: 1, name: "Three.js" },
+            { id: 2, name: "GSAP" },
+            { id: 3, name: "Next.js" },
+            { id: 4, name: "Tailwindcss" },
+        ],
+    },
+    ];
+
+export const socials = [
+    { name: "LinkedIn", href: "https://www.linkedin.com/in/sammy-pudke" },
+    { name: "GitHub", href: "https://github.com/Samy-rgb" },
+];
